311dc963a766bae578008da3c6bf0ca1e8e81388
Nelson Kelem <nelson@21waves.com>
2013-06-03 10:02:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockist::Application.config.secret_token = '7e0f368915ca78368e2df603c1a99fcd51b14d5586c44c1ae92441e102534f7dc9e27844cd9ec0d37e926f88a085b7907a0cc2841e1fb36cba130e6c9f0b067c'
